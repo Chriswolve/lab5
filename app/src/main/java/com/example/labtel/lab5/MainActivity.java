49470e46cbeaf9b7a41dee390bfe69988d345f12
Cristhianrg <cristhianr.gomez@gmail.com>
2016-11-05 15:29:18 -0500

    base
    base

---
+++ app/src/main/java/com/example/labtel/lab5/MainActivity.java
@@ -0,0 +1,31 @@
+package com.example.labtel.lab5;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.GridView;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        GridView gridview = (GridView) findViewById(R.id.gridview);
+        gridview.setAdapter(new ImageAdapter(this));
+
+
+        gridview.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            public void onItemClick(AdapterView<?> parent, View v, int position, long id) {
+                // Send intent to SingleViewActivity
+                Toast.makeText(getApplicationContext(), "Pelicula"+(position+1), Toast.LENGTH_LONG).show();
+            }
+        });
+    }
+
+
+}
